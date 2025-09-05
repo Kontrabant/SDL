5be232a6cc687cf0d06ac812a2f9d1ecff8bf1e8
Frank Praznik <frank.praznik@gmail.com>
2025-09-05 13:42:38 -0400

    locale: Use DBus to read and monitor the current locale

--- src/locale/unix/SDL_syslocale.c
+++ src/locale/unix/SDL_syslocale.c
@@ -22,6 +22,68 @@
 #include "SDL_internal.h"
 #include "../SDL_syslocale.h"
 
+#ifdef HAVE_DBUS_DBUS_H
+#include "../../core/linux/SDL_dbus.h"
+
+#define LOCALE_NODE "org.freedesktop.locale1"
+#define LOCALE_PATH "/org/freedesktop/locale1"
+#define LOCALE_INTERFACE "org.freedesktop.DBus.Properties"
+#define LOCALE_METHOD "Get"
+
+bool Locale_ReadDBusProperty()
+{
+    static const char *iface = LOCALE_NODE;
+    static const char *key = "Locale";
+
+    SDL_DBusContext *dbus = SDL_DBus_GetContext();
+
+    if (!dbus || !dbus->system_conn) {
+        return NULL;
+    }
+
+    DBusError err;
+    DBusMessage *reply = NULL;
+    DBusMessage *msg = dbus->message_new_method_call(LOCALE_NODE,
+                                                     LOCALE_PATH,
+                                                     LOCALE_INTERFACE,
+                                                     "Get"); // Method
+
+    if (msg) {
+        dbus->error_init(&err);
+        if (dbus->message_append_args(msg, DBUS_TYPE_STRING, &iface, DBUS_TYPE_STRING, &key, DBUS_TYPE_INVALID)) {
+            reply = dbus->connection_send_with_reply_and_block(dbus->system_conn, msg, DBUS_TIMEOUT_USE_DEFAULT, &err);
+        }
+        if (dbus->error_is_set(&err)) {
+            return NULL;
+        }
+        dbus->message_unref(msg);
+    }
+
+    if (reply) {
+        DBusMessageIter iter, item;
+        char *paths = NULL;
+
+        dbus->message_iter_init(reply, &iter);
+        dbus->message_iter_recurse(&iter, &iter);
+        dbus->message_iter_recurse(&iter, &item);
+
+        do {
+            int type = dbus->message_iter_get_arg_type(&item);
+
+            if (type == DBUS_TYPE_STRING) {
+                dbus->message_iter_get_basic(&item, &paths);
+                SDL_Log("LocaleStr: %s", paths);
+            }
+        } while (dbus->message_iter_next(&item));
+        //dbus->message_get_args(reply, &err, DBUS_TYPE_ARRAY, DBUS_TYPE_STRING, &paths, &path_count, DBUS_TYPE_INVALID);
+        dbus->message_unref(reply);
+    }
+
+    return reply;
+}
+
+#endif
+
 static void normalize_locale_str(char *dst, char *str, size_t buflen)
 {
     char *ptr;
@@ -68,6 +130,8 @@ bool SDL_SYS_GetPreferredLocales(char *buf, size_t buflen)
     bool isstack;
     const char *envr;
     char *tmp;
+
+    Locale_ReadDBusProperty();
 
     SDL_assert(buflen > 0);
     tmp = SDL_small_alloc(char, buflen, &isstack);
